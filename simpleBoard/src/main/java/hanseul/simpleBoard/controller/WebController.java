9bd78c4e09b014c27c978683ce5b20d8c98d9760
rhdiddl9537 <whanw7824@naver.com>
2023-03-16 03:20:05 +0900

    fixed untracked files

--- simpleBoard/src/main/java/hanseul/simpleBoard/controller/WebController.java
+++ simpleBoard/src/main/java/hanseul/simpleBoard/controller/WebController.java
@@ -9,4 +9,9 @@ public class WebController {
     public String getPostTitles() {
         return "posts";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
