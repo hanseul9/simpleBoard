f2134c9acf7471553fab55b21493b50723d52d6e
rhdiddl9537 <whanw7824@naver.com>
2023-03-18 00:17:30 +0900

    현재 로그인 회원 정보 조회 + 로그아웃 기능

--- simpleBoard/src/main/java/hanseul/simpleBoard/controller/MemberController.java
+++ simpleBoard/src/main/java/hanseul/simpleBoard/controller/MemberController.java
@@ -7,9 +7,14 @@ import hanseul.simpleBoard.responsedto.member.GetMemberResponseDto;
 import hanseul.simpleBoard.responsedto.member.JoinMemberResponse;
 import hanseul.simpleBoard.service.MemberService;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -36,6 +41,30 @@ public class MemberController {
         JoinMemberResponse joinMemberResponse = new JoinMemberResponse(memberId, CREATED, "회원가입 완료");
         return new ResponseEntity<>(joinMemberResponse, HttpStatus.CREATED);
     }
+
+    @GetMapping("/api/member") //로그인한 회원 조회 api
+    public ResponseEntity<BasicResponse> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        Optional<Member> memberOptional = memberService.findByEmail(email);
+
+        Member member = memberOptional.orElseThrow(() -> new EntityNotFoundException("회원 조회 실패"));
+
+        GetMemberResponseDto getMemberResponseDto = new GetMemberResponseDto(member);
+        BasicResponse basicResponse = new BasicResponse<>("회원 조회 성공", getMemberResponseDto);
+        return new ResponseEntity<>(basicResponse, HttpStatus.OK);
+    }
+    @PostMapping("/api/logout") //로그아웃
+    public ResponseEntity<BasicResponse> logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+        BasicResponse basicResponse = new BasicResponse<>("로그아웃 성공", null);
+        return ResponseEntity.ok(basicResponse);
+    }
+
 
 
     @GetMapping("/member/{memberId}") //회원 단건 조회
