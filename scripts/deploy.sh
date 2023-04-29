47fe65a5c7277b55440babe9b2ae77e374674f05
hanseul9 <102939057+hanseul9@users.noreply.github.com>
2023-04-29 19:13:48 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,10 +5,10 @@ PROJECT_NAME=simpleBoard/simpleBoard
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/simpleBoard/build/libs/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl simpleBoard | grep jar | awk '{print $1}')
 
 echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
@@ -28,6 +28,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-        -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+	-Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-real-db.properties,classpath:application-real.properties \
         -Dspring.profiles.active=real \
-        $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
