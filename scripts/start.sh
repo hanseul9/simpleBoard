b87dc06adae6b6b66dc2e014e8b9f703dd724df4
hanseul9 <102939057+hanseul9@users.noreply.github.com>
2023-04-30 03:22:53 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,23 +7,13 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-REPOSITORY=/home/ec2-user/app/deploy
-PROJECT_NAME=simpleBoard/simpleBoard
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=simpleBoard
 
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/simpleBoard/build/libs/*.jar $REPOSITORY/"
-cp $REPOSITORY/zip/simpleBoard/build/libs/*.jar $REPOSITORY/
+echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-# echo "> Build 파일 복사"
-# echo "> cp $REPOSITORY/zip/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/deploy/"
-
-# if ls $REPOSITORY/zip/$PROJECT_NAME/build/libs/*.jar 1> /dev/null 2>&1; then
-#   cp $REPOSITORY/zip/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/deploy/
-# else
-#   echo "> Build 파일이 존재하지 않습니다."
-#   exit 1
-# fi
-
+cp $REPOSITORY/deloy/*.jar $REPOSITORY/
 
 echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
@@ -40,7 +30,6 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.properties,file:/home/ec2-user/app/application-real.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-
