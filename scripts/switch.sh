91f96aced2ffa9ec47e68d757bd02bada31d6b0f
hanseul9 <102939057+hanseul9@users.noreply.github.com>
2023-05-02 21:55:01 +0900

    Update switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,8 +9,14 @@ source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
+    CURRENT_PORT=$(sudo nginx -T | grep -oP "(?<=listen\s)[0-9]+")
+    echo "> 현재 Port: $CURRENT_PORT"
 
-    echo "> 전환할 Port: $IDLE_PORT"
+    if [ "$IDLE_PORT" == "$CURRENT_PORT" ]; then
+        echo "> Port 가 변경되지 않아 nginx reload 건너뜁니다."
+        return
+    fi
+
     echo "> Port 전환"
     # nginx와 연결한 주소 생성
     # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
