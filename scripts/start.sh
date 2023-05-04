6f1b4dab143d9e6f92d5735020296f54191e23fd
hanseul9 <102939057+hanseul9@users.noreply.github.com>
2023-05-04 21:17:50 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,8 @@
 # start.sh
 # 서버 구동을 위한 스크립트
 
+export PATH=$PATH:/usr/local/jdk-17.0.7/bin
+
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
@@ -28,13 +30,12 @@ echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
-# echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-# nohup java -jar \
-#     -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-real-db.properties \
-#     -Dspring.profiles.active=$IDLE_PROFILE \
-#     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
+nohup java -jar \
+    -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
-nohup sh -c 'export PATH=$PATH:/usr/local/jdk-17.0.7/bin && java -jar -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &' > $REPOSITORY/nohup.out 2>&1 &
 
 
 # for PROFILE in "real1" "real2"
