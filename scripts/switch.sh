a87e0320fe4d767b03c9f1e9511b18476283d67a
hanseul9 <102939057+hanseul9@users.noreply.github.com>
2023-05-21 08:16:14 +0900

    Update switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -9,14 +9,8 @@ source ${ABSDIR}/profile.sh
 
 function switch_proxy() {
     IDLE_PORT=$(find_idle_port)
-    CURRENT_PORT=$(sudo nginx -T | grep -oP "(?<=listen\s)[0-9]+")
-    echo "> 현재 Port: $CURRENT_PORT"
 
-    if [ "$IDLE_PORT" == "$CURRENT_PORT" ]; then
-        echo "> Port 가 변경되지 않아 nginx reload 건너뜁니다."
-        return
-    fi
-
+    echo "> 전환할 Port: $IDLE_PORT"
     echo "> Port 전환"
     # nginx와 연결한 주소 생성
     # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
@@ -26,5 +20,25 @@ function switch_proxy() {
     # nignx reload. restart와는 다르게 설정 값만 불러옴
     sudo service nginx reload
 }
+
+# function switch_proxy() {
+#     IDLE_PORT=$(find_idle_port)
+#     CURRENT_PORT=$(sudo nginx -T | grep -oP "(?<=listen\s)[0-9]+")
+#     echo "> 현재 Port: $CURRENT_PORT"
+
+#     if [ "$IDLE_PORT" == "$CURRENT_PORT" ]; then
+#         echo "> Port 가 변경되지 않아 nginx reload 건너뜁니다."
+#         return
+#     fi
+
+#     echo "> Port 전환"
+#     # nginx와 연결한 주소 생성
+#     # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
+#     echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+#     echo "> 엔진엑스 Reload"
+#     # nignx reload. restart와는 다르게 설정 값만 불러옴
+#     sudo service nginx reload
+# }
 
 #switch_proxy
